243c24bf44818ea0f499fedb524d39fd1f0dd7df
Fang (MacPro) <faang1980@gmail.com>
2023-12-17 03:30:13 +1300

    Done for module

--- src/ReCAPTCHA.php
+++ src/ReCAPTCHA.php
@@ -2,6 +2,8 @@
 
 namespace Fstudio\Recaptcha;
 
+use Exception;
+
 class ReCAPTCHA{
 
     private $secret;
@@ -15,25 +17,31 @@ class ReCAPTCHA{
         $this->url = $_url;
     }
 
-    public function verify()
+    public function verify(String $ip = null)
     {
+        //To setup data 
+        $data = [
+            'secret'   => $this->secret,
+            'response' => $this->client_response
+        ];
+
+        if($ip)
+            $data['remoteip'] = $ip;
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         curl_setopt($ch, CURLOPT_POST, TRUE);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, [
-            'secret'   => $this->secret,
-            'response' => $this->client_response
-        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
         $output = curl_exec($ch);
         curl_close($ch);
 
-        if(!$output){
-            die("ERROR");
+        if($output){
+            throw new Exception("[Google reCAPTCHA] CURL post request error");
         }
 
         $response = json_decode($output);
